8a4a9faf9676f46df7db1b8e02a354d482c63d40
Ivan <kiosya.2016@mail.ru>
2019-05-16 01:12:04 +0500

    Первое домашнее задание

--- Lesson 1/js.js
+++ Lesson 1/js.js
@@ -1,1 +1,25 @@
-.
+let money =  prompt ( " Ваш бюджет на месяц? " , ' ' ),
+    time =  prompt ( ' Введите дату в формате YYYY-MM-DD ' , ' ' );
+
+let appData = {
+	budget : money,
+	costs : {},
+	nonexpenses : {},
+	income : [],
+	timeData : time,
+	saving :  false
+};
+
+let a1 =  prompt ( " Введите обязательную статью расходов в этом месяце " , ' ' ),
+	a2 =  prompt ( " Во сколько обойдется? " , ' ' ),
+	a3 =  prompt ( " Введите обязательную статью расходов в этом месяце " , ' ' ),
+	a4 =  prompt ( " Во сколько обойдется? " , ' ' );
+
+alert(appData.costs.а1 = a2);
+alert(appData.costs.а3 = a4);
+alert(parseInt(money) / 30);
+
+
+
+
+    
