f8b09497c99c770e513fc246caa226ce224f6bbf
Ivan <kiosya.2016@mail.ru>
2019-05-16 20:09:09 +0500

    Мое второе домашнее задание

--- Lesson 1/js.js
+++ Lesson 1/js.js
@@ -1,4 +1,4 @@
-let money =  prompt ( " Ваш бюджет на месяц? " , ' ' ),
+let money =  +prompt ( " Ваш бюджет на месяц? " , ' ' ),
     time =  prompt ( ' Введите дату в формате YYYY-MM-DD ' , ' ' );
 
 let appData = {
@@ -10,16 +10,50 @@ let appData = {
 	saving :  false
 };
 
-let a1 =  prompt ( " Введите обязательную статью расходов в этом месяце " , ' ' ),
-	a2 =  prompt ( " Во сколько обойдется? " , ' ' ),
-	a3 =  prompt ( " Введите обязательную статью расходов в этом месяце " , ' ' ),
-	a4 =  prompt ( " Во сколько обойдется? " , ' ' );
+//let i = 0;
+//while(i < 2) {
+//	console.log(i);
+//	i++;
 
-alert(appData.costs.а1 = a2);
-alert(appData.costs.а3 = a4);
-alert(parseInt(money) / 30);
+//do {
+//	console.log(i);
+//	i++;
+//}
+//	while(i < 2);
+
+for (let i = 0; i < 2; i++) {
+	let a =  prompt ( " Введите обязательную статью расходов в этом месяце " , ' ' ),
+		b =  prompt ( " Во сколько обойдется? " , ' ' );
+
+		if((typeof (a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+		&& a != " " && a.length < 50) {
+		console.log ("done");
+		appData.nonexpenses[а] = b;
+		} else {
+			alert(a);
+			console.log("Да ты блядь Стив Джобс!");
+		};
+	
+	}
+appData.moneyPerDey = appData.budget / 30;
+
+alert('Ежедневный расход: ' + appData.moneyPerDey);
+
+if(appData.moneyPerDey < 100) {
+	console.log("Минимальный уровень достатка");
+} else if(appData.moneyPerDey > 100 && appData.moneyPerDey < 2000) {
+	console.log('Средний уровень достатка');
+} else if(appData.moneyPerDey > 2000) {
+	console.log('Высокий уровень достатка');
+} else {
+	console.log('Произошла ошибка');
+}
+
 // вроде готово
 
+
+
+	
 
 
     
